038990e8578eab48f8dcdb81e02a906301775103
yapqh <qianhui.yap.2010@sis.smu.edu.sg>
2013-03-01 15:52:23 +0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset='utf-8'>
+    <title>Walk-a-City by TravelGeeks</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">  
+    <link href="/geektest/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/geektest/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/geektest/assets/css/docs.css" rel="stylesheet">
+    
+    <style>
+      .navbar-inner {background:#191919; color:#FE6E4C; font-weight: bold; padding: 5px; line-height: 10;}
+      h3 {margin: 10px 0 10px 0; color:#FFEBCD; font-family: "Architects Daughter",Comic Sans MS, serif}
+      
+    </style>
+   
+  </head>
+
+  <body style="background-color:#FFE4C4">
+
+    <div id="content-wrapper">
+      <div class="inner clearfix">
+        <section id="main-content">
+          <div class="row-fluid">  
+            <div class="span12">  
+              <img src="http://i1310.photobucket.com/albums/s647/skylarian/logo_slant_zps4d172dcf.png">
+              <p><img src="http://i1310.photobucket.com/albums/s647/skylarian/select_city_zps4638fcdf.png"></p>
+            </div>
+          </div>
+          
+          <form class="well" style="background:#FDF5E6">
+            <label style="font-style:bold; font-family:'Calibri',sans serif; color:#000080; font-size:large" >Country:</label>
+            <select>
+              <option value="" selected>--country--</option>
+              <option value="What">France</option>
+              <option value="ever">Germany</option>
+              <option value="value">Sweden</option>
+              <option value="needed">Spain</option>
+            </select>
+            <label style="font-style:bold; font-family:'Calibri',sans serif; color:#000080; font-size:large">City:</label>
+            <select>
+              <option value="" selected>--city--</option>
+              <option value="What">Paris</option>
+              <option value="ever">Lyon</option>
+              <option value="value">Toulouse</option>
+              <option value="needed">Bordeux</option>
+            </select>
+            <a href="http://travelgeeks.github.com/walkacity/paris.html" >
+            <button class="btn" style="background:#2C3939; color:#FDF5E6">Explore!</button>
+            </a>
+          </form>
+
+    </body>
+          
+</html>
